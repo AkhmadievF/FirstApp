db7311cc49da01e5a29d9942d3f4d8dbec9c4638
AkhmadievF <Fa.90@bk.ru>
2025-07-07 16:40:23 +0500

    addHomeWork

---
+++ src/productCard/PositionCode.kt
@@ -0,0 +1,8 @@
+package productCard
+
+enum class PositionCode(val title: String) {
+    DIRECTOR("Director"),
+    ACCOUNTANT("Accountant"),
+    ASSISTANT("Assistant"),
+    CONSULTANT("Consultant")
+}
